cfe98b88a7089f6d8015b006d9ff5160f9da055c
Fabiano Carvalho <fbiano15@hotmail.com>
2025-04-24 21:03:04 -0300

    .

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,5 @@
+// Menu Hâmburguer
+
 var hamburguer = document.getElementById("hamburguer")
 var menu = document.getElementById("menu")
 
@@ -7,4 +9,20 @@ hamburguer.addEventListener("click", function() {
     } else {
         menu.style.display = "none"
     }
+})
+
+// Botão Voltar ao Topo
+
+var botaotopo = document.getElementById("btntopo")
+
+window.addEventListener("scroll", function(){
+    if(document.documentElement.scrollTop > 300){
+        botaotopo.style.display = "block";
+    } else {
+        botaotopo.style.display = "none";
+    }
+    })
+
+botaotopo.addEventListener("click", function(){
+    window.scrollTo({top: 0, behavior: "smooth"}); // objeto
 })
